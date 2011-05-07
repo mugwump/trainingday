bf2f84b5b0553d2e453795f50b5bc0e3916816f8
Stefan Frank <s.frank@vierundsechzig.de>
2011-05-07 07:01:41 +0200

    Initial version of suspender-run for trainingday.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trainingday::Application.config.secret_token = 'dfe820dc4ccac00188bb1f91d0dab93c6ef77358e67f507a8799bec97e1bfed72b872469439f4da7136593a5f99c06540af982251561b298ae6e80af257e72dd'
---
+++ features/trainings/training_crud.feature
@@ -0,0 +1,6 @@
+Feature: Create and edit a Training
+
+  In order to make a Training available to others
+  As a signed-in user
+  I want to create a new Training
+
